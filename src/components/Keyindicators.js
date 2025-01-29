767d11f81eaaf5f27bf265b52015cfa3c2eee6eb
signature13s <mukulks1409@gmail.com>
2025-01-29 17:01:40 +0530

    Keyindicator done

--- src/components/Keyindicators.js
+++ src/components/Keyindicators.js
@@ -1,25 +1,22 @@
 import React from "react";
 import NavBar from "./NavBar";
 import Footer from "./footer";
-import key_pic from "../documents/Key_Indicators.jpg"
-import "./Keyindicators.css"
+import key_pic from "../documents/DST_Technology_Enabling_Centre-images-0.jpg";
+import "./Keyindicators.css";
 
-const Key_Indicator=()=>{
-    return(
-
-       <>
-       <NavBar/>
-       <div className="key_main">
-        <div className="key_heading">
-Key Indicators
-        </div>
+const Key_Indicator = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="key_main">
+        <div className="key_heading">Key Indicators</div>
         <div className="key_image">
-        <img src={key_pic} alt="reloaded"/>
+          <img src={key_pic} alt="reloaded" />
         </div>
-       </div>
-       <Footer/>
-       </> 
-    )
-}
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default Key_Indicator;
